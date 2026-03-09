b94ff463c21fe8652fdc3f35fe0dd6d5fe139a28
K <unknownork13@gmail.com>
2026-03-09 16:28:07 +0700

    v1.2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFontDatabase::addApplicationFont(":/fonts/Roboto-Italic-VariableFont_wdth,wght.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/Roboto-VariableFont_wdth,wght.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/Verdana.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/Verdana-Bold.ttf");
     a.setQuitOnLastWindowClosed(false);
     MainWindow w;
     w.show();
     return a.exec();
 }
+
